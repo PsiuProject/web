31f0c055fbb1301a0eb34a4e12f9969e3b22648f
Psiu <project@psiu.com>
2026-03-25 13:59:36 -0300

    build with /Web/ base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/Web/',
   build: {
     assetsDir: 'assets',
     assetsInlineLimit: 4096
